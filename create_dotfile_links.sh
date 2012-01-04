dcab6ebc553eeb4fe7b36dbdd76c90a40409ba60
Ryan Smith <rsmith@bandwidth.com>
2012-01-04 15:07:21 -0500

    add script to create the ~/ symlinks

---
+++ create_dotfile_links.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s ./.dotfiles/gitconfig ~/.gitconfig
+ln -s ./.dotfiles/gvimrc.before ~/.gvimrc.before
+ln -s ./.dotfiles/gvimrc.after ~/.gvimrc.after
+ln -s ./.dotfiles/vimrc.before ~/.vimrc.before
+ln -s ./.dotfiles/vimrc.after ~/.vimrc.after
+ln -s ./.dotfiles/profile ~/.profile
